3bf3f63824e9babeb89356aaa672f921eba4bb5d
caiofabioa <caiofabio@outlook.com>
2013-06-04 20:27:24 -0300

    Modelo TipoVinculo.java adicionado para popular campo ComboBox

---
+++ src/modelo/TipoVinculo.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class TipoVinculo {
+	
+	private int indice;
+	
+	private String descricao;
+
+	public int getIndice() {
+		return indice;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
